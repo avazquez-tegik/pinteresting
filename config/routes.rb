4ab3da5f5d6cab708ec3ff988bbd5210b42d9bfa
Alejandro Vazquez <mavm@outlook.com>
2015-02-11 22:49:16 -0600

    Contenedor2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #get 'pages/home'
    root "pages#home"
    get "about" => "pages#about"
+   get "carrusel" => "pages#carrusel"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
